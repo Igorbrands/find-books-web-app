32e762cc7b440509c19a47f9649b8c779bf8b085
igorbrands <igorcantelmo@gmail.com>
2021-06-09 04:11:22 -0300

    feat: global configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/global';
+import { Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+
+import { createBrowserHistory } from 'history';
 
 function App() {
+  const history = createBrowserHistory();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Router>
   );
 }
 
