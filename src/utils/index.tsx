3c48fe7fb40366540ba4ed4598b91ec4aa5219f9
igorbrands <igorcantelmo@gmail.com>
2021-06-18 01:45:39 -0300

    feat: creates format text function

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,12 @@
-export default function formatAuthors(authors: [string]) {
+function formatAuthors(authors: [string]) {
   authors.join();
 }
+
+function removeTags(str: string | undefined) {
+  if (str === null || str === '') return false;
+  else str = str?.toString();
+
+  return str?.replace(/(<([^>]+)>)/gi, '');
+}
+
+export { formatAuthors, removeTags };
