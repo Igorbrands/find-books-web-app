abbda40ffdd37be08cf8ebc482935edec10265f5
igorbrands <igorcantelmo@gmail.com>
2021-06-18 01:48:22 -0300

    feat: home books link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { ReactComponent as Hand } from '../../assets/hand.svg';
 import { ReactComponent as SearchIcon } from '../../assets/search.svg';
 
@@ -19,23 +19,20 @@ function Home() {
   const [searchInput, setSearchInput] = useState('');
 
   const history = useHistory();
-
-  function handleSearch() {
-    history.push(`search/${searchInput}`);
+  function handleSearch(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (searchInput.length > 0) {
+      history.push(`search/${searchInput}`);
+    }
+    return null;
   }
 
   return (
     <>
       <Container>
         <section>
-          <SearchBar>
-            <button
-              type="button"
-              onClick={(e) => {
-                e.preventDefault();
-                handleSearch();
-              }}
-            >
+          <SearchBar onSubmit={handleSearch}>
+            <button type="submit">
               <SearchIcon />
             </button>
             <input
@@ -43,7 +40,7 @@ function Home() {
               value={searchInput}
               placeholder="Search book"
               onChange={(e) => setSearchInput(e.target.value)}
-              onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+              onKeyPress={(e) => e.key === 'Enter' && handleSearch}
             />
           </SearchBar>
         </section>
@@ -57,10 +54,12 @@ function Home() {
             <h3>Discover new book</h3>
             <span>More</span>
           </SectionHeader>
-          <Banner>
-            <img src={images.banner1} alt="" />
-            <img src={images.banner4} alt="" />
-          </Banner>
+          <a href="http://localhost:3000/book/dsz5AwAAQBAJ">
+            <Banner>
+              <img src={images.banner1} alt="" />
+              <img src={images.banner4} alt="" />
+            </Banner>
+          </a>
         </section>
         <section>
           <SectionHeader
@@ -71,18 +70,22 @@ function Home() {
             <h3>Currently Reading</h3>
             <span>All</span>
           </SectionHeader>
-          <Banner style={{ position: 'relative', left: '-20px' }}>
-            <img src={images.banner2} alt="" />
-          </Banner>
+          <a href="http://localhost:3000/book/eLRhDgAAQBAJ">
+            <Banner style={{ position: 'relative', left: '-20px' }}>
+              <img src={images.banner2} alt="" />
+            </Banner>
+          </a>
         </section>
         <section>
           <SectionHeader>
             <h3>Reviews of The Days</h3>
             <span>All Video</span>
           </SectionHeader>
-          <Banner>
-            <img src={images.banner3} alt="" />
-          </Banner>
+          <a href="http://localhost:3000/book/QlduAgAAQBAJ">
+            <Banner>
+              <img src={images.banner3} alt="" />
+            </Banner>
+          </a>
         </section>
       </Container>
       <BottomNavigation />
